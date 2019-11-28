81616bbed8e612eb6d7197155db46d9da31b96a6
David Keathley <keathleydavidj@gmail.com>
2019-11-27 22:16:43 -0800

    Change AWSM theme to black

--- serum.exs
+++ serum.exs
@@ -1,10 +1,10 @@
 %{
-  site_name: "Blog",
-  site_description: "TODO: Add site description",
+  site_name: "Navigating Logic",
+  site_description: "The blog of an eccentric multi-billionaire thought leader and entrepreneur on a mission to CHANGE THE WORLD",
   date_format: "{WDfull}, {D} {Mshort} {YYYY}",
   base_url: "/",
-  author: "John Doe",
-  author_email: "john.doe@example.com",
+  author: "David Keathley",
+  author_email: "keathleydavidj@gmail.com",
   theme: Serum.Themes.Awsm,
   plugins: [
     {Serum.Plugins.LiveReloader, only: :dev}
